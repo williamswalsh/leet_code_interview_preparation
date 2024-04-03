124cb31fd8e0f6542ab78b6628ea5ea25eb97ed0
William Walsh <william.stephen.walsh@gmail.com>
2024-04-03 16:14:40 +1100

    Add brute force implementation to Two Sum question & non-working hashmap solution.

---
+++ src/test/java/top_150_interview_qs/Q1TwoSumTest.java
@@ -0,0 +1,95 @@
+package top_150_interview_qs;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+/**
+ * <pre>
+ * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ * You can return the answer in any order.
+ *
+ * Constraints:
+ * 2 <= nums.length <= 104
+ * -109 <= nums[i] <= 109
+ * -109 <= target <= 109
+ * Only one valid answer exists.
+ *
+ * Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
+ * - Can't sort numbers as need to identify element by its original index.??
+ * </pre>
+ */
+public class Q1TwoSumTest {
+
+    /**
+     * <pre>
+     * This is my brute force implementation.
+     *
+     * Leetcode:
+     * - 44ms        - beats 40.38% of users with Java
+     * - 44.37MB     - beats 94.98% of users with Java
+     * </pre>
+     */
+    public int[] twoSumBruteForce(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[]{};
+    }
+
+    //    Reuses the existing value
+//    like if its 3, 2, 4 t=6
+//    will give array indexes [0,0]
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>(nums.length);
+
+
+        // put all values
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                return new int[]{i, map.get(target - nums[i])};
+            }
+        }
+        return new int[]{};
+    }
+
+    @Test
+    void eg1Test() {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] expected = {0, 1};
+        int[] actual = twoSum(nums, target);
+        assertArrayEquals(expected, actual);
+    }
+
+
+    @Test
+    void eg2Test() {
+        int[] nums = {3, 2, 4};
+        int target = 6;
+        int[] expected = {1, 2};
+        int[] actual = twoSum(nums, target);
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void eg3Test() {
+        int[] nums = {3, 3};
+        int target = 6;
+        int[] expected = {0, 1};
+        int[] actual = twoSum(nums, target);
+        assertArrayEquals(expected, actual);
+    }
+}
